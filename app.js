3207be37fb098e60a9a2746cca13ec4dbe511fe5
Ahmad Shahzad <ahmad@shahzad.dev>
2023-10-26 20:39:18 +0500

    feat: nunjucks for templating engine.

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 require('dotenv').config();
 
+const njk = require('nunjucks');
 const express = require('express');
 
 const app = express();
 const PORT = process.env.UNIFIED_PORT || 49500;
 const HOST = process.env.UNIFIED_HOST || '127.0.0.1';
 
+njk.configure('./views', {
+    express: app,
+    autoescape: true,
+});
+app.set('view engine', 'html');
+
 app.get('/', (req, res) => {
-    res.send('Hello, express!');
+    res.render('home/home');
 });
 
 app.listen(PORT, () => {
