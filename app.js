7c02898332255d6264d754b1103b19b737983547
Ahmad Shahzad <ahmad@shahzad.dev>
2023-10-26 20:24:00 +0500

    feat: simple express app.

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+const PORT = process.env.UNIFIED_PORT || 49500;
+const HOST = process.env.UNIFIED_HOST || '127.0.0.1';
+
+app.get('/', (req, res) => {
+    res.send('Hello, express!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Unified Engine is listening at: http://${HOST}:${PORT}`);
+});
