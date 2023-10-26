c1aafd881a8247dec792bc6613dc223dc3ef86d9
Ahmad Shahzad <ahmad@shahzad.dev>
2023-10-27 00:38:01 +0500

    feat: set active class for active tab switch.

--- public/js/home/switchTab.js
+++ public/js/home/switchTab.js
@@ -6,4 +6,12 @@ const switchTab = (tabID) => {
         tab.classList.add('hidden');
         if (tab.id === tabID) tab.classList.remove('hidden');
     }
+
+    const switches = document.querySelectorAll('.switch');
+
+    for (let i = 0; i < switches.length; i++) {
+        const switche = switches[i];
+        switche.classList.remove('active');
+        if (switche.getAttribute('data-switch-id') === tabID) switche.classList.add('active');
+    }
 };
