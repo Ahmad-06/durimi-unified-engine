1272ae305ee6b8f28312bd913467ea46f6924374
Ahmad Shahzad <ahmad@shahzad.dev>
2023-10-27 10:44:32 +0500

    feat: embedded frame.

--- router/public/home/home.js
+++ router/public/home/home.js
@@ -29,5 +29,30 @@ HOME.get('/', async (req, res) => {
         }
     }
 
-    res.render('home/home', { apps, types: ['facebook', 'twitter'], enview: process.env.ENABLED_VIEW });
+    let app_active;
+
+    try {
+        const query = `
+            SELECT
+                *
+            FROM
+                apps
+            WHERE
+                type = ?
+            ORDER BY
+                priority;
+        `;
+        const params = [process.env.ENABLED_VIEW];
+
+        app_active = await db.get(query, params);
+    } catch (err) {
+        if (err) {
+            await db.close();
+            console.log(err);
+
+            // res.status(500).render('error/error', { error: err });
+        }
+    }
+
+    res.render('home/home', { apps, types: ['facebook', 'twitter'], enview: process.env.ENABLED_VIEW, app_active });
 });
