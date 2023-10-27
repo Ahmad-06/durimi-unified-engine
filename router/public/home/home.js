2abd47199ded7d187e03f32d2289cf73c03bc56a
Ahmad Shahzad <ahmad@shahzad.dev>
2023-10-27 09:34:13 +0500

    feat: passing the apps from the database to the frontend.

--- router/public/home/home.js
+++ router/public/home/home.js
@@ -3,6 +3,31 @@ const express = require('express');
 const HOME = express.Router();
 module.exports = HOME;
 
-HOME.get('/', (req, res) => {
-    res.render('home/home');
+const openDB = require('../../../data/openDB');
+
+HOME.get('/', async (req, res) => {
+    const db = await openDB();
+
+    let apps;
+
+    try {
+        const query = `
+            SELECT
+                *
+            FROM
+                apps
+            ORDER BY
+                priority
+        `;
+
+        apps = await db.all(query);
+    } catch (err) {
+        if (err) {
+            await db.close();
+
+            res.status(500).render('error/error', { error: err });
+        }
+    }
+
+    res.render('home/home', { apps, types: ['facebook', 'twitter'], enview: process.env.ENABLED_VIEW });
 });
